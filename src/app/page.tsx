9df936b1067df5eae8a1f7d9cbefdc14563a442f
gonakano <g.nakano555@gmail.com>
2024-04-17 00:01:38 +0800

    set back home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,32 @@ import parse from 'html-react-parser';
 
 export default function Home() {
   const [htmlString, setHtmlString] = useState('');
-  // const client = new ApolloClient({
-  //   uri: process.env.NEXT_PUBLIC_HYPGRAPH_URL,
-  //   cache: new InMemoryCache(),
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  // });
+  const client = new ApolloClient({
+    uri: process.env.NEXT_PUBLIC_HYPGRAPH_URL,
+    cache: new InMemoryCache(),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-  // client
-  //   .query({
-  //     query: gql`
-  //       query MyQuery {
-  //         news_all(first: 10) {
-  //           id
-  //           publishedAt
-  //           title
-  //           content {
-  //             html
-  //           }
-  //         }
-  //       }
-  //     `,
-  //   })
-  //   .then((result) => {
-  //     setHtmlString(result.data.news_all[0].content.html);
-  //   });
+  client
+    .query({
+      query: gql`
+        query MyQuery {
+          news_all(first: 10) {
+            id
+            publishedAt
+            title
+            content {
+              html
+            }
+          }
+        }
+      `,
+    })
+    .then((result) => {
+      setHtmlString(result.data.news_all[0].content.html);
+    });
 
-  return <main></main>;
+  return <main>{parse(htmlString)}</main>;
 }
